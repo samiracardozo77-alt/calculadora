09bf831690790db63449d99fe1180dcb37e3df37
Samira <you@example.com>
2026-04-28 10:38:02 -0300

    calculadora javascript

---
+++ script.js
@@ -0,0 +1,39 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorA11('button');
+
+buttons.forEach(btn=> {
+    btn.addEventListener('click', () => handleInput(btn.innerText));
+});
+
+function handleInput(value) {
+    if (value === 'C'){
+        display.innerText = '0' ;
+    } else if (value === '=') {
+        try{
+            display.innerText = eval(display.innerText
+                .replace('x', "*")
+                .replace('÷','/'));
+            } catch {
+                display.innerText === 'Erro';
+            }
+        }else{
+            if(display.innerText === '0') {
+                display.innerText = value;
+            } else {
+             display.innerText += value;
+        }
+    }
+}
+
+// suporte ao teclado 
+document.addEventtListener('keydown', (e) => {
+    if (!isNaN(e.key) || ['+','-','*','/','.'].includes(e.key)) {
+        handleInput(e.key);
+    } else if (e.key === 'Enter') {
+        handleInput('=');
+    } else if (e.key === 'Backpace') {
+        display.innerText = display.innerText.slice(0, -1)|| '0';
+    } else if (e.key.toLowerCase() === 'C') {
+        handleInput('C');
+    }
+});
